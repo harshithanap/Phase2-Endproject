edc52af41905f58a631f733d28b3be5b21567d3c
harshithanap <ashwathharshitha0@gmail.com>
2024-01-03 21:12:29 +0530

    project setup for PhaseEndProject

---
+++ src/main/java/com/harshi/starhealth/App.java
@@ -0,0 +1,13 @@
+package com.harshi.starhealth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"8df5e8e2-d029-4d48-bc73-399515327e52","feature":"Phase End project","scenario":"Star health home page test","start":1704295584668,"group":1,"content":"","tags":"","end":1704295619357,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
